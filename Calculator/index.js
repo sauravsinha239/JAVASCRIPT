b2d0c1a1b2b1585e552df2da879cd2a8d879797d
Saurav Kumar <106179718+sauravsinha239@users.noreply.github.com>
2025-03-23 14:44:36 +0000

    calculatorApp

---
+++ Calculator/index.js
@@ -0,0 +1,25 @@
+//Calculator
+
+const display = document.getElementById("display");
+
+function appendToDisplay(input) {
+
+    display.value += input;
+}
+
+function clearScreen() {
+
+    display.value = " ";
+}
+
+function calculate() {
+    try {
+
+        display.value = eval(display.value);
+    }
+    catch (error) {
+        display.value = "Error";
+
+    }
+
+}
